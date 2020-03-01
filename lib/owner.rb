8cd67793841906974cf43148d55852505aa9b283
Beth Ruff <beth.ruff2@gmail.com>
2020-03-01 20:06:07 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species 
+  
+  @@owners = []
+  
+  def initialize(name, species = "human")
+    @name = name
+    @species = species 
+    @@owners << self 
+  end 
+  
+  def say_species
+    "I am a #{@species}."
+  end 
+  
+  def self.all 
+    @@owners
+  end 
+  
+  def self.count
+    @@owners.count 
+  end 
+  
+  def self.reset_all
+    @@owners.clear.count
+  end 
+  
 end
